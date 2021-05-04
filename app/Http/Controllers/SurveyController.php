f76eaa5c3a2d80e60dbbe1b5254faa9370d05cab
Jonathan-Valentino <jonathanvalentinoprawira@yahoo.com>
2021-05-05 03:50:02 +0700

    Update Controller

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -2,18 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use App\Models\gender;
-use App\Models\interest;
-use App\Models\job;
-use App\Models\User;
-use App\Models\survey;
-use App\Models\user_interest;
-use App\Models\user_job;
 use Illuminate\Http\Request;
+use App\Models\survey;
 use Illuminate\Support\Facades\Auth;
 
-class UserController extends Controller
+class SurveyController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -22,22 +15,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $user = Auth::user();
-        $genders = gender::all();
-        $jobs = job::all();
-        $interests = interest::all();
-
-        $mygender = Auth::gender_id();
-        $myjob = Auth::job_id();
-        $myinterest = Auth::interest_id();
-        //$surveys = survey::all()->where('gender_id', $mygender);
-        $surveys = survey::all()->where([
-            ['gender_id', '=', $mygender],
-            ['job_id', '=', $myjob],
-            ['interest_id', '=', $myinterest],
-        ]);
-
-        return view('surveyor.dashboard', compact('genders', 'jobs', 'interests', 'user', 'surveys'));
+        //
     }
 
     /**
@@ -58,21 +36,13 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        $id = Auth::id();
-        $user = User::findOrFail($id);
-        $user->update([
+        survey::create([
+            'user_id' => Auth::id(),
+            'interest_id' => $request->interest,
+            'job_id' => $request->job,
             'gender_id' => $request->gender,
-            'is_survey_avail' => '1'
-        ]);
-
-        user_interest::create([
-            'user_id' => $request->gender,
-            'interest_id' => $request->interest
-        ]);
-
-        user_job::create([
-            'user_id' => $request->gender,
-            'job_id' => $request->job
+            'pay' => $request->pay,
+            'limit' => $request->limit,
         ]);
 
         return redirect()->route('user.index');
