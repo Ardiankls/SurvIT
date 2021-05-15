8522240e9869fa57712b9b1610afbed75e77951d
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2021-05-16 01:20:49 +0700

    Update dashboard.blade.php

--- resources/views/surveyor/dashboard.blade.php
+++ resources/views/surveyor/dashboard.blade.php
@@ -32,118 +32,113 @@
                                 <th scope="col">Aksi</th>
                             </tr>
                             </thead>
+                            <tbody>
+                            @foreach ($surveys as $survey)
+                                <?php
+                                $sinterests = $survey->interests;
+                                $sjobs = $survey->jobs;
+                                $sgender = $survey->gender_id;
+                                $sprovinces = $survey->provinces;
+                                ?>
+                                @foreach ($sinterests as $sinterest)
+                                @endforeach
+                                @foreach ($sjobs as $sjob)
+                                @endforeach
+                                @foreach ($sprovinces as $sprovince)
+                                @endforeach
+                                @foreach ($uinterests as $uinterest)
+                                @endforeach
+                                @foreach ($ujobs as $ujob)
+                                @endforeach
+                                @foreach ($uprovinces as $uprovince)
+                                @endforeach
 
-                        </table>
+                                @if ($sgender == $ugender || $sgender == '1')
+                                    @if ($sinterest->pivot->interest_id == $uinterest->pivot->interest_id || $sinterest->pivot->interest_id == '1')
+                                        @if ($sjob->pivot->job_id == $ujob->pivot->job_id || $sjob->pivot->job_id == '1')
+                                            @if ($sprovince->pivot->province_id == $uprovince->pivot->province_id || $sprovince->pivot->province_id == '1')
+                                                <tr class="text-center">
+                                                    <td>
+                                                        {{ $survey->title }}
+                                                    </td>
+                                                    <td>
+                                                        @if($sinterest->interest == 'Tidak ada')
+                                                            Umum
+                                                        @else
+                                                            {{ $sinterest->interest }}
+                                                        @endif
 
-                    @endif
+                                                    </td>
+                                                    <td>
+                                                        {{-- @foreach($usurveys as $surv)
+                                                                @if($surv->survey_id == $survey->id)
+                                                                    {{$surv}}
+                                                                    @if($surv != null)
+                                                                        @if($surv->status == 2)
+                                                                            Proses
+                                                                        @elseif($surv->status == 3)
+                                                                            Sukses
+                                                                        @endif
+                                                                    @else
+                                                                        Baru
+                                                                    @endif                                                                    @endif
 
-                </div>
-                <div class="card rounded-lg">
-                    <table>
-                        <tbody>
-                        @foreach ($surveys as $survey)
-                            <?php
-                            $sinterests = $survey->interests;
-                            $sjobs = $survey->jobs;
-                            $sgender = $survey->gender_id;
-                            $sprovinces = $survey->provinces;
-                            ?>
-                            @foreach ($sinterests as $sinterest)
-                            @endforeach
-                            @foreach ($sjobs as $sjob)
-                            @endforeach
-                            @foreach ($sprovinces as $sprovince)
-                            @endforeach
-                            @foreach ($uinterests as $uinterest)
-                            @endforeach
-                            @foreach ($ujobs as $ujob)
-                            @endforeach
-                            @foreach ($uprovinces as $uprovince)
-                            @endforeach
-
-                            @if ($sgender == $ugender || $sgender == '1')
-                                @if ($sinterest->pivot->interest_id == $uinterest->pivot->interest_id || $sinterest->pivot->interest_id == '1')
-                                    @if ($sjob->pivot->job_id == $ujob->pivot->job_id || $sjob->pivot->job_id == '1')
-                                        @if ($sprovince->pivot->province_id == $uprovince->pivot->province_id || $sprovince->pivot->province_id == '1')
-                                            <tr class="text-center">
-                                                <td>
-                                                    {{ $survey->title }}
-                                                </td>
-                                                <td>
-                                                    @if($sinterest->interest == 'Tidak ada')
-                                                        Umum
-                                                    @else
-                                                        {{ $sinterest->interest }}
-                                                    @endif
-
-                                                </td>
-                                                <td>
-                                                    {{-- @foreach($usurveys as $surv)
-                                                            @if($surv->survey_id == $survey->id)
-                                                                {{$surv}}
-                                                                @if($surv != null)
-                                                                    @if($surv->status == 2)
-                                                                        Proses
-                                                                    @elseif($surv->status == 3)
-                                                                        Sukses
+                                                        @endforeach --}}
+                                                        @if($survey->users != null)
+                                                            @foreach($survey->users as $usurveyy)
+                                                                @if($usurveyy->pivot->user_id == $user->id)
+                                                                    @if($usurveyy->exists())
+                                                                        @if($usurveyy->pivot->status == 2)
+                                                                            Proses
+                                                                        @elseif($usurveyy->pivot->status == 3)
+                                                                            Sukses
+                                                                        @endif
+                                                                    @else
+                                                                        Baru
                                                                     @endif
-                                                                @else
-                                                                    Baru
-                                                                @endif                                                                    @endif
-
-                                                    @endforeach --}}
-                                                    @if($survey->users != null)
-                                                        @foreach($survey->users as $usurveyy)
-                                                            @if($usurveyy->pivot->user_id == $user->id)
-                                                                @if($usurveyy->exists())
-                                                                    @if($usurveyy->pivot->status == 2)
-                                                                        Proses
-                                                                    @elseif($usurveyy->pivot->status == 3)
-                                                                        Sukses
-                                                                    @endif
-                                                                @else
-                                                                    Baru
                                                                 @endif
-                                                            @endif
-                                                        @endforeach
-                                                    @else
-                                                        Baru
-                                                    @endif
-                                                </td>
-                                                <td>
-                                                    {{-- {{ $survey->pay / $survey->limit }}pt --}}
-                                                    0pt
-                                                </td>
-                                                <td>
-                                                    <a href={{ $survey->link }} target="_blank"
-                                                       class="btn btn-primary">Buka</a>
-                                                </td>
-                                                <td>
-                                                    {{-- @if($survey->users != null)
-                                                        @if($usurveyy->pivot->user_id == $user->id)
-                                                            -
-                                                        @else --}}
-                                                    <form action="{{ route('usersurvey.update', $survey) }}"
-                                                          method="post" enctype="multipart/form-data">
-                                                        @csrf
-                                                        <input name="_method" type="hidden" value="PATCH">
-                                                        <button class="btn btn-primary" id="selesai" type="submit"
-                                                                style="background-color: rgb(221,177,226);">Selesai
-                                                        </button>
-                                                    </form>
-                                                    {{-- @endif
-                                                @endif --}}
-                                                </td>
-                                            </tr>
+                                                            @endforeach
+                                                        @else
+                                                            Baru
+                                                        @endif
+                                                    </td>
+                                                    <td>
+                                                        {{-- {{ $survey->pay / $survey->limit }}pt --}}
+                                                        0pt
+                                                    </td>
+                                                    <td>
+                                                        <a href={{ $survey->link }} target="_blank"
+                                                           class="btn btn-primary">Buka</a>
+                                                    </td>
+                                                    <td>
+                                                        {{-- @if($survey->users != null)
+                                                            @if($usurveyy->pivot->user_id == $user->id)
+                                                                -
+                                                            @else --}}
+                                                        <form action="{{ route('usersurvey.update', $survey) }}"
+                                                              method="post" enctype="multipart/form-data">
+                                                            @csrf
+                                                            <input name="_method" type="hidden" value="PATCH">
+                                                            <button class="btn btn-primary" id="selesai" type="submit"
+                                                                    style="background-color: rgb(221,177,226);">Selesai
+                                                            </button>
+                                                        </form>
+                                                        {{-- @endif
+                                                    @endif --}}
+                                                    </td>
+                                                </tr>
+                                            @endif
                                         @endif
                                     @endif
                                 @endif
-                            @endif
 
-                        @endforeach
-                        </tbody>
-                    </table>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+
                 </div>
+
             </div>
         </div>
     </div>
