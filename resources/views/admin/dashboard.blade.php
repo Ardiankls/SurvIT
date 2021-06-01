3f693a5a7cfa7147c324f44b00f4ac80f559c934
Jonathan-Valentino <jonathanvalentinoprawira@yahoo.com>
2021-06-01 19:08:26 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -32,7 +32,7 @@
                                         @endif
                                     </td>
                                     <td>
-                                        <form action="{{ route('usersurvey.update', $usurvey->id) }}" method="post"
+                                        <form action="{{ route('usersurvey.update', $usurvey) }}" method="post"
                                             enctype="multipart/form-data">
                                             @csrf
                                             <input name="_method" type="hidden" value="PATCH">
