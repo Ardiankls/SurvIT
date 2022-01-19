28ff711f7af67baae5df942876a186c690c79fe8
sirjvp <62706314+sirjvp@users.noreply.github.com>
2022-01-19 16:04:13 +0700

    Add mail blade

---
+++ resources/views/mail/penarikan_pengajuan.blade.php
@@ -0,0 +1,7 @@
+<div class="" style="height: 200px">
+    <h1>Pemberitahuan Penarikan Poin</h1>
+    <h3 style="margin-bottom: 15px">Kamu telah mengajukan penarikan poin sebanyak {{ $point }} ke rekening {{ $bank_account }}. Tolong hubungi Admin Survit jika ini bukan anda.</h3>
+    <a href="https://wa.me/6289644655003"
+       style="background-color: #007BFF; color: white; text-align: center; padding: 10px; border-radius: 10px; height: 20px; margin-top: 30px;">
+       Hubungi Kami</a>
+</div>
---
+++ resources/views/mail/penarikan_sukses.blade.php
@@ -0,0 +1,7 @@
+<div class="" style="height: 200px">
+    <h1>Penarikan Poin Sukses</h1>
+    <h3 style="margin-bottom: 15px">Kami telah mengirimkan poin sebanyak {{ $point }} ke rekening {{ $bank_account }}. Jika terjadi kesalahan tolong hubungi kami.</h3>
+    <a href="https://wa.me/6289644655003"
+       style="background-color: #007BFF; color: white; text-align: center; padding: 10px; border-radius: 10px; height: 20px; margin-top: 30px;">
+       Hubungi Kami</a>
+</div>
