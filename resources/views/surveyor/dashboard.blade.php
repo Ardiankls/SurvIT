7ca6f6c005fa569c30072794159f08a4722cca51
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2022-01-25 15:05:02 +0700

    Update dashboard.blade.php

--- resources/views/surveyor/dashboard.blade.php
+++ resources/views/surveyor/dashboard.blade.php
@@ -2,6 +2,7 @@
 @include('surveyor.modal.demographyModal')
 @include('surveyor.modal.pointModal')
 @section('content')
+
     <div class="container-xxl  p-5">
         <div class="row justify-content-center ">
             <div class="col-md-9"></div>
@@ -194,20 +195,5 @@
             });
         });
     </script>
-
-    {{-- Boostrap 5 --}}
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" --}}
-    {{-- integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"> --}}
-    {{-- </script> --}}
-    {{-- jQuery --}}
-    {{-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> --}}
-    {{-- DataTables --}}
-    {{-- <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap5.js"></script>
-    <script>
-        $(document).ready(function() {
-            var thetable = $('#myTable').DataTable({});
-        });
-    </script> --}}
 
 @endsection
