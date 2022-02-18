414c921e978283a2482173400d97545055022ab4
Ardian-Kurniawan <ardiankurniawan7@gmail.com>
2022-02-18 23:12:02 +0700

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,66 +4,160 @@
         <div class="row justify-content-center ">
             <div class="col-md-9"></div>
 
-            <div class="col-md-8 mt-5">
-                <div class="bg-white rounded-lg shadow d-none d-md-block" style="">
-                    <div>
-                        <div class="float-left pt-3 pl-3">
-                            <h4>Profil</h4>
-                        </div>
-                        <div class="float-right pt-3 pr-3">
-                            <a href="{{ route('user.edit', $user) }}" class="btn btn-primary">Ubah</a>
-                        </div>
-                    </div>
-                    <div class="p-5"></div>
-                    <div class="text-center">
-                        <img style="width: 20%;" src="/images/profile.png" alt="">
-                    </div>
-                    <div class="p-3"></div>
-                    <div class="col text-center">
-                        <h2>{{ $user->first_name }} {{ $user->last_name }}</h2>
-                        <h4>{{ $user->email }}</h4><br>
-                        Nama Depan: {{ $user->first_name }}<br>
-                        Nama Belakang: {{ $user->last_name }}<br>
-                        Nomor Telepon: {{ $user->phone }}<br>
-                    </div>
-                    <div class="p-5"></div>
-                </div>
-            </div>
+{{--            Old Ui--}}
+{{--            <div class="col-md-8 mt-5">--}}
+{{--                <div class="bg-white rounded-lg shadow d-none d-md-block" style="">--}}
+{{--                    <div>--}}
+{{--                        <div class="float-left pt-3 pl-3">--}}
+{{--                            <h4>Profil</h4>--}}
+{{--                        </div>--}}
+{{--                        <div class="float-right pt-3 pr-3">--}}
+{{--                            <a href="{{ route('user.edit', $user) }}" class="btn btn-primary">Ubah</a>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="p-5"></div>--}}
+{{--                    <div class="text-center">--}}
+{{--                        <img style="width: 20%;" src="/images/profile.png" alt="">--}}
+{{--                    </div>--}}
+{{--                    <div class="p-3"></div>--}}
+{{--                    <div class="col text-center">--}}
+{{--                        <h2>{{ $user->first_name }} {{ $user->last_name }}</h2>--}}
+{{--                        <h4>{{ $user->email }}</h4><br>--}}
+{{--                        Nama Depan: {{ $user->first_name }}<br>--}}
+{{--                        Nama Belakang: {{ $user->last_name }}<br>--}}
+{{--                        Nomor Telepon: {{ $user->phone }}<br>--}}
+{{--                    </div>--}}
+{{--                    <div class="p-5"></div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
 
-            <div class="col-md-8 mt-5">
-                <div class="bg-white rounded-lg shadow d-none d-md-block" style="">
-                    @if ($user->is_survey_avail == '0')
-                        <div>
-                            <div class="float-left pt-3 pl-3">
-                                <h4>Demografi</h4>
+{{--            NEW UI--}}
+            <div id="content" class=" p-md-5 pt-5">
+                <!-- <h2 class="mb-4">Sidebar #04</h2> -->
+                <div class="row">
+                    <div class="col-9">
+                        <div class="panel mr-3 px-4 py-3 glass shadow ">
+                            <div class="d-flex ">
+                                <h5 class="flex-grow-1">Profile</h5>
+                                <div class="btn btn-primary">
+                                    <a href="{{ route('user.edit', $user) }}" class="text-white px-2 ">Ubah</a>
+                                </div>
+                            </div>
+                            <div class="body">
+                                <div class="row mb-5 mt-3">
+                                    <div class="row mb-3">
+                                        <img class="mx-auto" src="/images/profile.png" alt="" style="width: 10em;">
+                                    </div>
+                                    <div class="row">
+                                        <h4 class="text-center">{{ $user->first_name }} {{ $user->last_name }}</h4>
+                                    </div>
+                                    <div class="row">
+                                        <p class="text-center">{{ $user->email }}</p>
+                                    </div>
+                                </div>
+                                <div class="row px-5">
+                                    <div class="row">
+                                        <div class="col-3 fs-5 text-dark ">
+                                            Poin:
+                                        </div>
+                                        <div class="col-6 fs-5 text-primary">
+                                            10,900
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-3 fs-5 text-dark ">
+                                            Surveys:
+                                        </div>
+                                        <div class="col-6 fs-5 text-primary">
+                                            5
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-3 fs-5 text-dark ">
+                                            join Date:
+                                        </div>
+                                        <div class="col-6 fs-5 text-primary">
+                                            25 Februari 2021
+                                        </div>
+                                        <div class="mb-2 ">
+                                            <button class="btn  btn-danger">logout</button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="p-5"></div>
-                        <div class="col text-center">
-                            Anda belum mengisi demografi
-                        </div>
-                        <div class="p-4"></div>
-                    @else
-                        <div>
-                            <div class="float-left pt-3 pl-3">
-                                <h4>Demografi</h4>
+
+                    </div>
+                    <div class="col-3">
+                        <div class="row mb-4">
+                            <div class=" panel glass shadow px-4 py-3">
+                                <div class="d-flex ">
+                                    <h5 class="flex-grow-1">Demography</h5>
+                                    <button class="btn btn-primary px-4 ">
+                                        <a href="{{ route('user.edit', $user) }}" class="text-white">edit</a>
+                                    </button>
+                                </div>
+                                <div class="body px-4 py-3">
+                                    <div class="row mb-4">
+                                        <h5>Pekerjaan</h5>
+                                        <div class="ml-2 text-dark">
+                                            <div> {{ $user->jobs->first()->job_name ?? 'null' }} </div>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-4">
+                                        <h5>Provinsi</h5>
+                                        <div class="ml-2 text-dark">
+                                            <div>{{ $user->province->province ?? 'null' }}</div>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-4">
+                                        <h5>Hobi/Kesukaan/Topik</h5>
+                                        <div class="ml-2 text-dark">
+                                            <div>{{ $user->interests->first()->interest ?? 'null' }}</div>
+                                            <div>Games</div>
+                                            <div>Teknologi</div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="float-right pt-3 pr-3">
-                                <a href="" class="btn btn-primary" id="edit" data-toggle="modal"
-                                    data-target="#editdemography">Ubah</a>
-                            </div>
                         </div>
-                        <div class="p-5"></div>
-                        <div class="col text-center">
-                            Jenis Kelamin: {{ $user->gender->gender ?? 'null' }}<br>
-                            Pekerjaan: {{ $user->jobs->first()->job_name ?? 'null' }}<br>
-                            Kesukaan / Topik / Hobi: {{ $user->interests->first()->interest ?? 'null' }}<br>
-                            Provinsi: {{ $user->province->province ?? 'null' }}<br>
-                        </div>
-                        <div class="p-4"></div>
-                    @endif
+                    </div>
                 </div>
             </div>
+{{--            <div class="col-md-8 mt-5">--}}
+{{--                <div class="bg-white rounded-lg shadow d-none d-md-block" style="">--}}
+{{--                    @if ($user->is_survey_avail == '0')--}}
+{{--                        <div>--}}
+{{--                            <div class="float-left pt-3 pl-3">--}}
+{{--                                <h4>Demografi</h4>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="p-5"></div>--}}
+{{--                        <div class="col text-center">--}}
+{{--                            Anda belum mengisi demografi--}}
+{{--                        </div>--}}
+{{--                        <div class="p-4"></div>--}}
+{{--                    @else--}}
+{{--                        <div>--}}
+{{--                            <div class="float-left pt-3 pl-3">--}}
+{{--                                <h4>Demografi</h4>--}}
+{{--                            </div>--}}
+{{--                            <div class="float-right pt-3 pr-3">--}}
+{{--                                <a href="" class="btn btn-primary" id="edit" data-toggle="modal"--}}
+{{--                                    data-target="#editdemography">Ubah</a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="p-5"></div>--}}
+{{--                        <div class="col text-center">--}}
+{{--                            Jenis Kelamin: {{ $user->gender->gender ?? 'null' }}<br>--}}
+{{--                            Pekerjaan: {{ $user->jobs->first()->job_name ?? 'null' }}<br>--}}
+{{--                            Kesukaan / Topik / Hobi: {{ $user->interests->first()->interest ?? 'null' }}<br>--}}
+{{--                            Provinsi: {{ $user->province->province ?? 'null' }}<br>--}}
+{{--                        </div>--}}
+{{--                        <div class="p-4"></div>--}}
+{{--                    @endif--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </div>
     </div>
     <div class="modal fade" id="editdemography" tabindex="-1" role="dialog">
