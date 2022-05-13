45ab872c0b653c175ec3dfe01f4567b627892aba
Ardian-Kurniawan <56443318+Ardiankls@users.noreply.github.com>
2022-05-13 10:59:52 +0700

    Update guestModal.blade.php

--- resources/views/surveyor/modal/guestModal.blade.php
+++ resources/views/surveyor/modal/guestModal.blade.php
@@ -3,18 +3,17 @@
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Peringatan</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">Isi Survey Dapet UANG!</h5>
                 <button type="button" class="close" data-dismiss="modal" id="myBtn" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body text-justify text-dark">
-                Jika anda ingin mendapatkan poin dengan mengisi survei ini, segera daftarkan diri anda di
-                Survit!
+                Kamu bisa dapetin uang lho dengan cara isi survei ini, Yuk segera buat akun kamu di Survit!
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal" id="myBtn2">Tidak dulu</button>
-                <a href="{{ route('register') }}" class="btn btn-primary">Register Sekarang</a>
+                <a href="{{ route('register') }}" class="btn btn-primary">Daftar Sekarang</a>
             </div>
         </div>
     </div>
